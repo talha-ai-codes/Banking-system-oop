d9cb06697fc5e4c578a7b2087bffe6ebd55e3d5a
talha-ai-codes <mt3708722@gmail.com>
2026-03-28 14:51:04 +0500

    add steps

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int a,b;
+    cout << "enter a and b: ";
+    cin >> a >> b;
+    cout << "==before swaping==" << endl;
+    cout << "a : " << a << endl;
+    cout << "b  : " << b << endl;
+    cout << "==swap a and b ===" << endl;
+    int c = a;
+    a = b;
+    b = c;
+    cout << "===after swaping===" << endl;
+    cout << "a : " << a  << endl;
+    cout << "b  : " << b << endl;
+    return 0;
     
 }
